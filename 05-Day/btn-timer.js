8aa3c5b31169965c98bd2e92f51061902d657ef0
PIYUSHDAHLE <piyushdahle@gmail.com>
2025-01-23 12:31:02 +0530

    DOM Topics

--- 05-Day/btn-timer.js
+++ 05-Day/btn-timer.js
@@ -1,5 +1,5 @@
 const h1 = document.querySelector('h1')
-const span = document.querySelector('span')
+const span = document.querySelector('#s1')
 const box = document.querySelector('.box')
 function getRandomRGBColor() {
     const r = Math.floor(Math.random() * 256); 
@@ -12,19 +12,25 @@ function getRandomRGBColor() {
 let timer = 3;
 
 const interval = setInterval(()=>{
-
+    
     --timer;
-
+    
     if (timer === 0) {
         h1.innerHTML = '<button> Click Me </button>'
         
         clearInterval(interval)
-
+        
         const btn = document.querySelector('button')
         
         btn.addEventListener('click',()=>{
             box.style.backgroundColor = getRandomRGBColor()
-           })
+        })
     }
     span.textContent = timer
 },1000)
+
+const word = document.querySelector('#s2');
+window.addEventListener('keypress', (event) => {
+    word.textContent = event.key; 
+});
+
